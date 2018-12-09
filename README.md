f36fef80914af96e445a784732a1d69013f1395c
Kursat Kobya <kursat.kobya@gmail.com>
2018-12-09 18:35:03 +0100

    day1: Add second solution

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # adventofcode
-adventofcode
+This repo consist c++ solutions for www.adventofcode.com
+
+# Dependencies
+cmake
+g++
+
+# Build
+`mkdir build && cd build && cmake .. && make`
+
--- day1/side_A.cpp
+++ day1/side_A.cpp
@@ -1,43 +1,74 @@
+#include <climits>
 #include <iostream>
 #include <fstream>
+#include <unordered_map>
 #include <string>
+#include <vector>
 
 class Solution {
 public:
     Solution(std::string filePath) :
         m_input {filePath, std::ios::binary},
-        m_solution {0}
+        m_solutionA {0},
+        m_solutionB {INT_MIN}
     {
         if (m_input.fail()) {
             std::cout << "Error opening " << filePath;
         }
     }
 
-    int solve();
+    int solveA();
+    int solveB();
 private:
-    std::ifstream m_input;
-    int           m_solution;
+    std::ifstream      m_input;
+    std::vector<int>   m_inputVector;
+    int                m_solutionA;
+    int                m_solutionB;
+    std::unordered_map<int, int> m_frequency;
 };
 
-int Solution::solve()
+int Solution::solveA()
 {
     std::string str;
     while (std::getline(m_input, str))
     {
-        auto diff = std::stoi(str.substr(1));
-        if (str[0] == '-') {
-            m_solution -= diff;
-        } else {
-            m_solution += diff;
+        auto diff = std::stoi(str);
+        m_inputVector.push_back(diff);
+        m_solutionA += diff;
+        if (m_frequency.count(m_solutionA) > 0) {
+            m_solutionB = m_solutionA;
+        }
+        m_frequency[m_solutionA]++;
+    }
+
+    return m_solutionA;
+}
+
+int Solution::solveB()
+{
+    int diff{m_solutionA};
+    while (m_solutionB == INT_MIN)
+    {
+        for (auto anomaly : m_inputVector) {
+            diff += anomaly;
+            if (m_frequency.count(diff) > 0) {
+                 m_solutionB = diff;
+                break;
+            }
+            m_frequency[diff]++;
         }
     }
 
-    return m_solution;
+    return m_solutionB;
 }
 
 int main()
 {
-    Solution side_A{"../input"};
-    std::cout << side_A.solve() << std::endl;
+    std::cout << "Side A" << std::endl;
+    Solution side_A{"../day1/input"};
+    std::cout << side_A.solveA() << std::endl;
+
+    std::cout << "Side B" << std::endl;
+    std::cout << side_A.solveB() << std::endl;
     return 0;
 }
