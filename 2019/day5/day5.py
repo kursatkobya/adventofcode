96b9e0da518eaaf261f2341a9060ac0a1f3b35af
Kursat Kobya <KKobya@luxoft.com>
2019-12-09 18:25:31 +0100

    day5: change input method to convert int right away

--- 2019/day5/day5.py
+++ 2019/day5/day5.py
@@ -1,68 +1,62 @@
 #!/usr/bin/python3
+from typing import List, Iterable
 
 
-with open("input", "r") as day2_input:
-    input_array = day2_input.readline().split(",")
-
-
-def get_param(index: int, mode: int) -> int:
+def get_param(input_array: List[int], index: int, mode: int) -> int:
     if mode == 0:
-        return int(input_array[int(input_array[index])])
-    return int(input_array[index])
+        return input_array[input_array[index]]
+    return input_array[index]
 
 
-def alarma():
+def alarma(input_array: List[int]):
     index = 0
-    while int(input_array[index]) is not 99:
-        opmodes = (int(int(input_array[index]) / 100) % 10,
-                   int(int(input_array[index]) / 1000) % 10,
-                   int(int(input_array[index]) / 10000) % 10)
-        opcode = int(input_array[index]) % 100
+    while input_array[index] is not 99:
+        opmodes = (int(input_array[index] / 100) % 10,
+                   int(input_array[index] / 1000) % 10)
+        opcode = input_array[index] % 100
         if opcode == 1:
-            parameter1 = get_param(index+1, opmodes[0])
-            parameter2 = get_param(index+2, opmodes[1])
-            input_array[int(input_array[index+3])] = str(parameter1 +
-                                                         parameter2)
+            parameter1 = get_param(input_array, index+1, opmodes[0])
+            parameter2 = get_param(input_array, index+2, opmodes[1])
+            input_array[input_array[index+3]] = parameter1 + parameter2
             index += 4
         elif opcode == 2:
-            parameter1 = get_param(index+1, opmodes[0])
-            parameter2 = get_param(index+2, opmodes[1])
-            input_array[int(input_array[index+3])] = str(parameter1 *
-                                                         parameter2)
+            parameter1 = get_param(input_array, index+1, opmodes[0])
+            parameter2 = get_param(input_array, index+2, opmodes[1])
+            input_array[input_array[index+3]] = parameter1 * parameter2
             index += 4
         elif opcode == 3:
-            input_array[int(input_array[index+1])] = int(input("Shoot!: "))
+            input_array[input_array[index+1]] = int(input("Shoot!: "))
             index += 2
         elif opcode == 4:
-            print(input_array[int(input_array[index+1])])
+            print(input_array[input_array[index+1]])
             index += 2
         elif opcode == 5:
-            parameter1 = get_param(index+1, opmodes[0])
-            parameter2 = get_param(index+2, opmodes[1])
+            parameter1 = get_param(input_array, index+1, opmodes[0])
+            parameter2 = get_param(input_array, index+2, opmodes[1])
             if parameter1 is not 0:
                 index = parameter2
             else:
                 index += 3
         elif opcode == 6:
-            parameter1 = get_param(index+1, opmodes[0])
-            parameter2 = get_param(index+2, opmodes[1])
+            parameter1 = get_param(input_array, index+1, opmodes[0])
+            parameter2 = get_param(input_array, index+2, opmodes[1])
             if parameter1 is 0:
                 index = parameter2
             else:
                 index += 3
         elif opcode == 7:
-            parameter1 = get_param(index+1, opmodes[0])
-            parameter2 = get_param(index+2, opmodes[1])
-            parameter3 = int(input_array[index+3])
+            parameter1 = get_param(input_array, index+1, opmodes[0])
+            parameter2 = get_param(input_array, index+2, opmodes[1])
+            parameter3 = input_array[index+3]
             if parameter1 < parameter2:
                 input_array[parameter3] = 1
             else:
                 input_array[parameter3] = 0
             index += 4
         elif opcode == 8:
-            parameter1 = get_param(index+1, opmodes[0])
-            parameter2 = get_param(index+2, opmodes[1])
-            parameter3 = int(input_array[index+3])
+            parameter1 = get_param(input_array, index+1, opmodes[0])
+            parameter2 = get_param(input_array, index+2, opmodes[1])
+            parameter3 = input_array[index+3]
             if parameter1 == parameter2:
                 input_array[parameter3] = 1
             else:
@@ -70,4 +64,7 @@ def alarma():
             index += 4
 
 
-alarma()
+data: List[int]
+with open('input') as f:
+    data = [int(num) for num in f.read().strip().split(',')]
+    alarma(data)
